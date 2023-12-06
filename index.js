a379992834fcfcad7514758213754c56aaef6ab3
Alban CRESTEE <150236274+AlbanoCresto@users.noreply.github.com>
2023-12-06 17:40:01 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+alert("Vous êtes la plus belle femme du monde ! Utilisez votre charme à bon escient !");
+
+function showPopup(){
+    alert("Si tu ouvres ta case une fois que tu es au havre, tu gagne un massage de 20min en bonus !");
+}
+
+function showPopupGift021223(){
+    alert("Tu as déjà reçu ce cadeau sur whatsapp !");
+}
+
+function showPopupGift041223(){
+    alert("Aujourd'hui ton cadeau est un indice : féminisme, 24, Jeff");
+}
