97e2e499d4b5744079146a22318a0e97bc1ccd4e
Alban CRESTEE <150236274+AlbanoCresto@users.noreply.github.com>
2023-12-10 21:23:09 +0100

    Update index.js

--- index.js
+++ index.js
@@ -15,3 +15,7 @@ function showPopupGift041223(){
 function showPopupGift081223(){
     alert("Aujourd'hui ton cadeau est un deuxième indice : QUOI, QUAND, COMMENT");
 }
+
+function showPopupGift101223(){
+    alert("Aujourd'hui, c'est juste des mots qui font du bien, c'est un petit cadeau mais moi je trouve que tu mérites ce cadeau. Tu me supporte, tu me soutiens, t'es déterminée dans ce que tu fais, t'es la meilleure des chérie, alors tu mérites des mots doux. Parce que je veux que tu te souviennes toujours, que t'es la plus belle, que t'es la plus agréable, que t'es la plus marrante, que t'es très attentionnée, que t'as de belles valeurs et surtout que personne ne t'arrive à la cheville à mes yeux. Voilà c'est tout ce que je voulais te dire aujourd'hui, t'as plein de qualités ma chérie, ça va aller, je t'aime fort d'amour");
+}
